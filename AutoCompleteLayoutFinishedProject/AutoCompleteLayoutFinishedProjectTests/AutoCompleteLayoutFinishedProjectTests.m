959adb6d894e54732acc65c889e90cbe8dced756
Benson Huynh <quoc@shaw.ca>
2015-10-27 18:12:59 -0700

    Final revision

---
+++ AutoCompleteLayoutFinishedProject/AutoCompleteLayoutFinishedProjectTests/AutoCompleteLayoutFinishedProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  AutoCompleteLayoutFinishedProjectTests.m
+//  AutoCompleteLayoutFinishedProjectTests
+//
+//  Created by Benson Huynh on 2015-10-27.
+//  Copyright © 2015 Benson Huynh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoCompleteLayoutFinishedProjectTests : XCTestCase
+
+@end
+
+@implementation AutoCompleteLayoutFinishedProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
